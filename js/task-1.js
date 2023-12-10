cddf4fa7ecc90f1b585e15daa4961579eb765e7a
Modelist17 <hunteruboat@gmail.com>
2023-12-10 21:44:05 +0100

    bobo update

--- js/task-1.js
+++ js/task-1.js
@@ -1,23 +1,43 @@
 
-// task 4.1:
-function isEnoughCapacity(products, containerSize) {
-  // Отримуємо масив ключів об'єкта products
-  const productNames = Object.keys(products);
+// task 5.1:
+const getUserNames = (users) => users.map(user => user.name);
 
-  // Отримуємо загальну кількість товарів у контейнері за допомогою reduce
-  const totalProducts = productNames.reduce((acc, productName) => {
-    // Отримуємо кількість товару за ключем productName
-    const quantity = products[productName];
-    // Додаємо кількість товару до акумулятора
-    return acc + quantity;
-  }, 0);
-
-  // Перевіряємо, чи загальна кількість товарів не перевищує розмір контейнера
-  return totalProducts <= containerSize;
-}
-
-// Перевірка коректності роботи функції
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));  // true
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));   // false
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));   // true
-console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
+console.log(
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764
+    },
+  ])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
